9602c7fc3210e89800fecfd0d1b5c63db1cc17f4
JustOneDeveloper | Benni <78445321+Just1Developer@users.noreply.github.com>
2024-12-18 11:28:21 +0100

    Added Project + First Bug

---
+++ src/main/java/net/justonedev/bug/calendar1/Main.java
@@ -0,0 +1,10 @@
+package net.justonedev.bug.calendar1;
+
+// No visibility modifier to keep class visible in the package only
+class Main {
+
+    public static void main(String[] args) {
+        // Todo find bug
+    }
+
+}
